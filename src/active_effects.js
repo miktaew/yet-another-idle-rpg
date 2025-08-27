0971374091decca57b4a7a99f7de2657a847c337
miktaew <49405403+miktaew@users.noreply.github.com>
2025-08-27 23:08:20 +0200

    Update active_effects.js

--- src/active_effects.js
+++ src/active_effects.js
@@ -32,160 +32,179 @@ class ActiveEffect {
 }
 
 //weather effects
-effect_templates["Wet"] = new ActiveEffect({
-    name: "Wet",
-    effects: {
-        stats: {
-            cold_tolerance: {flat: -5},
-        }
-    },
-});
+(()=>{
+    effect_templates["Wet"] = new ActiveEffect({
+        name: "Wet",
+        effects: {
+            stats: {
+                cold_tolerance: {flat: -5},
+            }
+        },
+    });
 
-effect_templates["Cold"] = new ActiveEffect({
-    name: "Cold",
-    effects: {
-        stats: {
-            stamina_efficiency: {multiplier: 0.8},
-            strength: {multiplier: 0.9},
-        }
-    },
-    group_tags: {cold: 1},
-});
-effect_templates["Very cold"] = new ActiveEffect({
-    name: "Very cold",
-    effects: {
-        stats: {
-            stamina_efficiency: {multiplier: 0.6},
-            dexterity: {multiplier: 0.8},
-            agility: {multiplier: 0.8},
-            strength: {multiplier: 0.8},
-        }
-    },
-    group_tags: {cold: 2},
-});
-effect_templates["Freezing"] = new ActiveEffect({
-    name: "Freezing",
-    effects: {
-        stats: {
-            stamina_efficiency: {multiplier: 0.3},
-            dexterity: {multiplier: 0.6},
-            agility: {multiplier: 0.6},
-            strength: {multiplier: 0.6},
-            health_regeneration_flat: {multiplier: 0.6},
-            health_regeneration_percent: {multiplier: 0.6},
+    effect_templates["Cold"] = new ActiveEffect({
+        name: "Cold",
+        effects: {
+            stats: {
+                stamina_efficiency: {multiplier: 0.8},
+                strength: {multiplier: 0.9},
+            }
         },
-        xp_multipliers: {
-            all: 0.6,
-        }
-    },
-    group_tags: {cold: 3},
-});
-effect_templates["Hypothermia"] = new ActiveEffect({
-    name: "Hypothermia",
-    effects: {
-        stats: {
-            stamina_efficiency: {multiplier: 0.1},
-            dexterity: {multiplier: 0.3},
-            agility: {multiplier: 0.3},
-            strength: {multiplier: 0.4},
-            health_regeneration_flat: {multiplier: 0.2},
-            health_regeneration_percent: {multiplier: 0.2},
+        group_tags: {cold: 1},
+    });
+    effect_templates["Very cold"] = new ActiveEffect({
+        name: "Very cold",
+        effects: {
+            stats: {
+                stamina_efficiency: {multiplier: 0.6},
+                dexterity: {multiplier: 0.8},
+                agility: {multiplier: 0.8},
+                strength: {multiplier: 0.8},
+            }
         },
-        xp_multipliers: {
-            all: 0.3,
-        }
-    },
-    group_tags: {cold: 4},
-});
+        group_tags: {cold: 2},
+    });
+    effect_templates["Freezing"] = new ActiveEffect({
+        name: "Freezing",
+        effects: {
+            stats: {
+                stamina_efficiency: {multiplier: 0.3},
+                dexterity: {multiplier: 0.6},
+                agility: {multiplier: 0.6},
+                strength: {multiplier: 0.6},
+                health_regeneration_flat: {multiplier: 0.6},
+                health_regeneration_percent: {multiplier: 0.6},
+            },
+            xp_multipliers: {
+                all: 0.6,
+            }
+        },
+        group_tags: {cold: 3},
+    });
+    effect_templates["Hypothermia"] = new ActiveEffect({
+        name: "Hypothermia",
+        effects: {
+            stats: {
+                stamina_efficiency: {multiplier: 0.1},
+                dexterity: {multiplier: 0.3},
+                agility: {multiplier: 0.3},
+                strength: {multiplier: 0.4},
+                health_regeneration_flat: {multiplier: 0.2},
+                health_regeneration_percent: {multiplier: 0.2},
+            },
+            xp_multipliers: {
+                all: 0.3,
+            }
+        },
+        group_tags: {cold: 4},
+    });
+})();
 /////////////////////////////////////////
 
+//consumables' effects
+(()=>{
+    effect_templates["Basic meal"] = new ActiveEffect({
+        name: "Basic meal",
+        effects: {
+            stats: {
+                stamina_regeneration_flat: {flat: 1},
+            }
+        },
+        tags: {"buff": true},
+    });
 
-effect_templates["Basic meal"] = new ActiveEffect({
-    name: "Basic meal",
+    effect_templates["Weak healing powder"] = new ActiveEffect({
+        name: "Weak healing powder",
+        effects: {
+            stats: {
+                health_regeneration_flat: {flat: 2},
+            }
+        },
+        tags: {"buff": true, "medicine": true},
+    });
+    effect_templates["Healing powder"] = new ActiveEffect({
+        name: "Healing powder",
+        effects: {
+            stats: {
+                health_regeneration_flat: {flat: 5},
+            }
+        },
+        tags: {"buff": true, "medicine": true},
+    });
+    effect_templates["Weak healing potion"] = new ActiveEffect({
+        name: "Weak healing potion",
+        effects: {
+            stats: {
+                health_regeneration_flat: {flat: 8},
+                health_regeneration_percent: {flat: 1},
+            }
+        },
+        tags: {"buff": true, "medicine": true},
+    });
+    effect_templates["Healing potion"] = new ActiveEffect({
+        name: "Healing potion",
+        effects: {
+            stats: {
+                health_regeneration_flat: {flat: 20},
+                health_regeneration_percent: {flat: 2},
+            }
+        },
+        tags: {"buff": true, "medicine": true},
+    });
+    effect_templates["Weak healing balm"] = new ActiveEffect({
+        name: "Weak healing balm",
+        effects: {
+            stats: {
+                health_regeneration_flat: {flat: 5},
+                health_regeneration_percent: {flat: 0.5},
+            }
+        },
+        tags: {"buff": true, "medicine": true},
+    });
+
+    effect_templates["Cheap meat meal"] = new ActiveEffect({
+        name: "Cheap meat meal",
+        effects: {
+            stats: {
+                stamina_regeneration_flat: {flat: 2},
+            }
+        },
+        tags: {"buff": true},
+    });
+    effect_templates["Simple meat meal"] = new ActiveEffect({
+        name: "Simple meat meal",
+        effects: {
+            stats: {
+                stamina_regeneration_flat: {flat: 3},
+                health_regeneration_flat: {flat: 1},
+            }
+        },
+        tags: {"buff": true},
+    });
+    effect_templates["Slight food poisoning"] = new ActiveEffect({
+        name: "Slight food poisoning",
+        effects: {
+            stats: {
+                health_loss_flat: {flat: -0.5},
+            }
+        },
+        tags: {"debuff": true, "poison": true},
+    });
+})();
+
+
+//
+effect_templates["Spark of Inspiration"] = new ActiveEffect({
+    name: "Spark of Inspiration",
     effects: {
-        stats: {
-            stamina_regeneration_flat: {flat: 1},
+        xp_multipliers: {
+            all: 1.5,
         }
     },
     tags: {"buff": true},
 });
 
-effect_templates["Weak healing powder"] = new ActiveEffect({
-    name: "Weak healing powder",
-    effects: {
-        stats: {
-            health_regeneration_flat: {flat: 2},
-        }
-    },
-    tags: {"buff": true, "medicine": true},
-});
-effect_templates["Healing powder"] = new ActiveEffect({
-    name: "Healing powder",
-    effects: {
-        stats: {
-            health_regeneration_flat: {flat: 5},
-        }
-    },
-    tags: {"buff": true, "medicine": true},
-});
-effect_templates["Weak healing potion"] = new ActiveEffect({
-    name: "Weak healing potion",
-    effects: {
-        stats: {
-            health_regeneration_flat: {flat: 8},
-            health_regeneration_percent: {flat: 1},
-        }
-    },
-    tags: {"buff": true, "medicine": true},
-});
-effect_templates["Healing potion"] = new ActiveEffect({
-    name: "Healing potion",
-    effects: {
-        stats: {
-            health_regeneration_flat: {flat: 20},
-            health_regeneration_percent: {flat: 2},
-        }
-    },
-    tags: {"buff": true, "medicine": true},
-});
-effect_templates["Weak healing balm"] = new ActiveEffect({
-    name: "Weak healing balm",
-    effects: {
-        stats: {
-            health_regeneration_flat: {flat: 5},
-            health_regeneration_percent: {flat: 0.5},
-        }
-    },
-    tags: {"buff": true, "medicine": true},
-});
 
-effect_templates["Cheap meat meal"] = new ActiveEffect({
-    name: "Cheap meat meal",
-    effects: {
-        stats: {
-            stamina_regeneration_flat: {flat: 2},
-        }
-    },
-    tags: {"buff": true},
-});
-effect_templates["Simple meat meal"] = new ActiveEffect({
-    name: "Simple meat meal",
-    effects: {
-        stats: {
-            stamina_regeneration_flat: {flat: 3},
-            health_regeneration_flat: {flat: 1},
-        }
-    },
-    tags: {"buff": true},
-});
-effect_templates["Slight food poisoning"] = new ActiveEffect({
-    name: "Slight food poisoning",
-    effects: {
-        stats: {
-            health_loss_flat: {flat: -0.5},
-        }
-    },
-    tags: {"debuff": true, "poison": true},
-});
+
 
 export {effect_templates, ActiveEffect};
